d0938ce7b36dd4c8632b4d6a31ae8080b97c4d35
Hugo Ángeles <hugoangeles0810@gmail.com>
2018-09-29 11:29:17 -0500

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_hub/followers_screen.dart';
+import 'package:mobile_hub/following_screen.dart';
+import 'package:mobile_hub/repository_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MobileHub"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("MobileHub"),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(child: Text("Repositories")),
+              Tab(child: Text("Followers")),
+              Tab(child: Text("Following")),
+            ],
+          ),
+        ),
+        drawer: Drawer(),
+        body: TabBarView(
+          children: <Widget>[
+            RepositoryScreen(),
+            FollowersScreen(),
+            FollowingScreen(),
+          ],
+        ),
       ),
-      drawer: Drawer(),
-      body: Container(),
     );
   }
 }
